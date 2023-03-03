17e73f5c750f46787210af5b4bb0499c603bfffb
alexxio1989 <alessiopinna.elis@gmail.com>
2023-03-03 17:44:10 +0100

    svil

--- projects/ap-fe-fundamentals-lib/src/lib/ap-fe-fundamentals-lib.module.ts
+++ projects/ap-fe-fundamentals-lib/src/lib/ap-fe-fundamentals-lib.module.ts
@@ -2,15 +2,29 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ImageCropperModule } from 'ngx-image-cropper';
 import { ImagesEditorComponent } from '../images-editor/images-editor.component';
-
-
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
   declarations: [
     ImagesEditorComponent
   ],
   imports: [
     CommonModule,
-    ImageCropperModule
+    ImageCropperModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatDialogModule
   ],
   exports: [
     ImagesEditorComponent
